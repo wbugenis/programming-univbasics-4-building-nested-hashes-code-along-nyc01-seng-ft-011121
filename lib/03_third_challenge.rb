9c5a9cb4a033af1ec49a2c35c4cda2fe0295551c
William Bugenis <wbugenis@gmail.com>
2020-12-08 17:23:07 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -3,19 +3,19 @@ def third_challenge
    :montague => {
       :patriarch => {
         #your key/value pairs here
-        name: "Lord Montague"
+        name: "Lord Montague",
         age: "53"
         },
       :matriarch => {
         #your key/value pairs here
-        name: "Lady Montague"
-        age: "53"
+        name: "Lady Montague",
+        age: "53",
         status: "alive"
         },
       :hero => {
         #your key/value pairs here
-        name: "Romeo"
-        age: "15"
+        name: "Romeo",
+        age: "15",
         status: "alive"
         
       },
@@ -24,18 +24,18 @@ def third_challenge
    :capulet => {
       :patriarch => {
         #your key/value pairs here
-        :name => "Lord Capulet"
+        :name => "Lord Capulet",
         :age => "50"
         },
       :matriarch => {
         #your key/value pairs here
-        :name => "Lady Capulet"
+        :name => "Lady Capulet",
         :age => "51"
         },
       :heroine => {
         #your key/value pairs here
-        :name => "Juliet"
-        :age => "15"
+        :name => "Juliet",
+        :age => "15",
         :status => "alive"
         },
       :heroine_friends => []

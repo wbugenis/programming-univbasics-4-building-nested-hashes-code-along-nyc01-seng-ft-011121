5818d3633309fdb74c7ba7ec4bc828a61f8146ef
William Bugenis <wbugenis@gmail.com>
2020-12-08 17:20:32 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -36,7 +36,7 @@ def third_challenge
         #your key/value pairs here
         :name => "Juliet"
         :age => "15"
-        :status: => "alive"
+        :status => "alive"
         },
       :heroine_friends => []
    }
